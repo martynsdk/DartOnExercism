777fb6239c18d24dfb9e0a6591a9d6e29872b038
martynfigueiredo <martyn.figueiredo@pearson.com>
2022-03-13 09:26:33 -0600

    Two Fer solution

---
+++ Exercises/Two Fer/two_fer.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  String twofer() {
+    String? name = stdin.readLineSync();
+  if (name == ""){
+    name = "you";
+  }
+  String twofer(name) => "One for $name, one for me.";
+  return twofer(name);
+
+  }
+  print (twofer());
+}
