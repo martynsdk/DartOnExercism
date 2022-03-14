4ab2b4cbd40929ee36f9c2d40fe7da7acfbe5158
martynfigueiredo <martyn.figueiredo@pearson.com>
2022-03-14 14:32:37 -0600

    Leap Year

---
+++ Exercises/Leap/leap.dart
@@ -0,0 +1,19 @@
+    import 'dart:io';
+
+    bool leapYear(int year){
+      return (year / 4) % 1 == 0 &&
+          ((year / 100) % 1 != 0 || (year / 400) % 1 == 0);
+      }
+
+    void main(){
+
+      print("Enter a year to check : ");
+      String? s = stdin.readLineSync();
+        int year = int.parse(s!);
+
+      if (leapYear(year)) {
+        print("Leap Year");
+      } else {
+        print("Not a Leap Year");
+      }
+    }
