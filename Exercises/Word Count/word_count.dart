975d416889e472611c077a7216040c48a2213508
Martyn Figueiredo <martynsdk@gmail.com>
2022-03-18 07:23:42 -0600

    Update word_count.dart

--- Exercises/Word Count/word_count.dart
+++ Exercises/Word Count/word_count.dart
@@ -1,1 +1,27 @@
-main(){}
+  import 'dart:io';
+
+  void main(){
+    print("Enter a phrase: ");
+    var entry = countWords(stdin.readLineSync()!);
+    print("The result of the sentence word counter is: ");
+    entry.forEach((key, value) {
+      print("$key = $value");
+    });
+  }
+
+  Map<String, int> countWords(String phrase) {
+    Map<String, int> map = {};
+    RegExp wordRegex =
+    RegExp(r"([a-z]+('[a-z])?)|([0-9]+)", caseSensitive: false);
+    wordRegex.allMatches(phrase).forEach((element) {
+      String word = element[0]!.toLowerCase();
+      if (map[word] != null) {
+        map[word] = map[word]! + 1;
+      }else{
+        map[word] = 1;
+      }
+    });
+    return map;
+  }
+
+
