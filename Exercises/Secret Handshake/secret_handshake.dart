c434defdbdf50bfcabb9893ca5c51664e187d122
Martyn SDK <martynsdk@gmail.com>
2022-04-18 10:50:30 -0300

    Secret Handshake

--- Exercises/Secret Handshake/secret_handshake.dart
+++ Exercises/Secret Handshake/secret_handshake.dart
@@ -1,17 +1,22 @@
-
-
-
 import 'dart:io';
 
-main(){
-
+main() {
   print('Please enter a number:');
-  int? a = int.parse(stdin.readLineSync()!);
-  var s = a.toRadixString(2);
+  int? givenNumber = int.tryParse(stdin.readLineSync()!);
 
-  print(s);
+  if (givenNumber != null) {
+    List<String> comparison(int value) {
+      List<String> result = [];
+      if (value & 1 == 1) result.add('wink');
+      if (value & 2 == 2) result.add('double blink');
+      if (value & 4 == 4) result.add('close your eyes');
+      if (value & 8 == 8) result.add('jump');
+      if (value & 16 == 16) result.reversed.toList();
+      return result;
+    }
 
-
-
-
+    print("Result: ${comparison(givenNumber)}");
+  } else {
+    print('The input is not a numeric string, please provide a valid number.');
+  }
 }
